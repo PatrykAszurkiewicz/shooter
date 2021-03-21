70a765d9fbc4224ed57241aee71f4970b37c499c
patrykasz <patrykos65a@gmail.com>
2021-03-21 23:11:03 +0100

    dodano gre

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed;
+    private Transform playerPos;
+    private Player player;
+    public GameObject Killeffect;
+
+    private RipplePostProcessor camRipple;
+
+    private Shake shake;
+    
+    
+
+    private void Start()
+    {
+        camRipple = Camera.main.GetComponent<RipplePostProcessor>();
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<Shake>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            Instantiate(Killeffect, transform.position, Quaternion.identity);
+            player.currentHealth--;
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("Projectile"))
+        {
+            Score.scoreValue += 1;
+            camRipple.RippleEffect();
+            shake.CamShake();
+            Instantiate(Killeffect, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/enemy3.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy3 : MonoBehaviour
+{
+    public float speed;
+    private Transform playerPos;
+    private Player player;
+    public GameObject Killeffect;
+    public int EnemyHealth;
+
+    private RipplePostProcessor camRipple;
+
+    private Shake shake;
+
+
+
+    private void Start()
+    {
+        camRipple = Camera.main.GetComponent<RipplePostProcessor>();
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<Shake>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            Instantiate(Killeffect, transform.position, Quaternion.identity);
+            player.currentHealth--;
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("Projectile"))
+        {
+            Destroy(other.gameObject);
+            camRipple.RippleEffect();
+            shake.CamShake();
+            EnemyHealth--;
+            if (EnemyHealth <= 0)
+            {
+                Score.scoreValue += 1;
+                Instantiate(Killeffect, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+
+        }
+    }
+
+}
